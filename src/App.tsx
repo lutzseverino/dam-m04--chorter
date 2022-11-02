b0a3b377324fe8157f9eab1d1f576cc155585356
jasperls <jasperlutzseverino@gmail.com>
2022-11-02 13:50:29 +0100

    style: add button examples

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,50 @@ const App = () => {
     <>
       <GlobalStyle />
       <div style={{ margin: "1rem", display: "flex", gap: "1rem" }}>
+        <Button size="large">Label</Button>
         <Button>Label</Button>
+        <Button size="small">Label</Button>
+        <Button size="large" disabled={true}>
+          Label
+        </Button>
         <Button disabled={true}>Label</Button>
-        <Button variant="stroke">Label</Button>
-        <Button variant="stroke" disabled={true}>
+        <Button size="small" disabled={true}>
+          Label
+        </Button>
+      </div>
+      <div style={{ margin: "1rem", display: "flex", gap: "1rem" }}>
+        <Button variant="secondary" size="large">
           Label
         </Button>
         <Button variant="secondary">Label</Button>
+        <Button variant="secondary" size="small">
+          Label
+        </Button>
+        <Button variant="secondary" size="large" disabled={true}>
+          Label
+        </Button>
         <Button variant="secondary" disabled={true}>
+          Label
+        </Button>
+        <Button variant="secondary" size="small" disabled={true}>
+          Label
+        </Button>
+      </div>
+      <div style={{ margin: "1rem", display: "flex", gap: "1rem" }}>
+        <Button variant="stroke" size="large">
+          Label
+        </Button>
+        <Button variant="stroke">Label</Button>
+        <Button variant="stroke" size="small">
+          Label
+        </Button>
+        <Button variant="stroke" size="large" disabled={true}>
+          Label
+        </Button>
+        <Button variant="stroke" disabled={true}>
+          Label
+        </Button>
+        <Button variant="stroke" size="small" disabled={true}>
           Label
         </Button>
       </div>
