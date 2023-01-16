af65b60ba6c943b03836e839cfaebabb0b69cfa3
jasperls <jasperlutzseverino@gmail.com>
2023-01-16 14:46:28 +0100

    feat: clear template and start styles

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import NavBar from "../components/navBar";
+import styles from "../styles/Main.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Chorter</title>
+        <meta name="description" content="Chores made shorter" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <NavBar />
+      </main>
+    </>
+  );
+}
