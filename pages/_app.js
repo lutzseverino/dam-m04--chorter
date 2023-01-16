8dd88700360e048f84d719c285030ebad86ce468
jasperls <jasperlutzseverino@gmail.com>
2023-01-16 14:47:22 +0100

    feat: set default font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Space_Grotesk } from "@next/font/google";
+
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${spaceGrotesk.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
