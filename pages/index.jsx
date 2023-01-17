7d587445372b7c036c2d1a13d2ee19fa174a3501
jasperls <jasperlutzseverino@gmail.com>
2023-01-17 18:36:40 +0100

    feat: migrate index to styled-components (partial)
    remove unnecessary css modules

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
-import NavBar from "../components/navBar";
+import NavBar from "../components/NavBar";
+
+import styled from "styled-components";
 import styles from "../styles/Main.module.css";
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default function Home() {
   return (
@@ -11,9 +20,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <NavBar />
-      </main>
+
+      <NavBar continued={true} />
+
+      <StyledMain>
+        {/* TODO: Migrate to styled-components */}
+        <div className={styles.action}>
+          <div>
+            <h1>Chores made shorter</h1>
+          </div>
+          <div>callout</div>
+        </div>
+      </StyledMain>
+
+      {/* <Footer /> */}
     </>
   );
 }
