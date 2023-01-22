a59cecbeb0d9b68af24f4d019ce2c45a129a6f13
zarcerog <nicostios24@gmail.com>
2023-01-22 23:24:06 +0100

    feat: :sparkles: add headings css settings

---
+++ components/Headings.jsx
@@ -0,0 +1,58 @@
+import "styled-components";
+
+const Heading1 = styled.h1`
+    display: block;
+    font-size: 2em;
+    margin-bottom: 0.67em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Heading2 = styled.h2`
+    display: block;
+    font-size: 1.5em;
+    margin-bottom: 0.83em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Heading3 = styled.h3`
+    display: block;
+    font-size: 1.17em;
+    margin-bottom: 1em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Heading4 = styled.h4`
+    display: block;
+    font-size: 1em;
+    margin-bottom: 1.33em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Heading5 = styled.h5`
+    display: block;
+    font-size: 0.83em;
+    margin-bottom: 1.67em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Heading6 = styled.h6`
+    display: block;
+    font-size: 0.67em;
+    margin-bottom: 2.33em;
+    font-weight: 700;
+    color: #000;
+`;
+
+const Paragraph = styled.p`
+    font-size: 1em;
+    margin-bottom: 1em;
+    font-weight: 400;
+    color: #1a1a1a;
+`;
+
+export { Heading1, Heading2, Heading3, Heading4, Heading5, Heading6, Paragraph };
