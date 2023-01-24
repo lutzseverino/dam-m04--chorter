d171185266ffdc0be036e49b867be33773a03f99
jasperls <jasperlutzseverino@gmail.com>
2023-01-24 18:12:56 +0100

    fix: headings styles

--- components/Headings.jsx
+++ components/Headings.jsx
@@ -1,58 +1,62 @@
-import "styled-components";
+import styled from "styled-components";
 
 const Heading1 = styled.h1`
-    display: block;
-    font-size: 2em;
-    margin-bottom: 0.67em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 2rem;
+  margin-bottom: 0.67rem;
+  font-weight: 700;
 `;
 
 const Heading2 = styled.h2`
-    display: block;
-    font-size: 1.5em;
-    margin-bottom: 0.83em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 1.5rem;
+  margin-bottom: 0.83rem;
+  font-weight: 700;
 `;
 
 const Heading3 = styled.h3`
-    display: block;
-    font-size: 1.17em;
-    margin-bottom: 1em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 1.17rem;
+  margin-bottom: 1rem;
+  font-weight: 700;
 `;
 
 const Heading4 = styled.h4`
-    display: block;
-    font-size: 1em;
-    margin-bottom: 1.33em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 1rem;
+  margin-bottom: 1.33rem;
+  font-weight: 700;
 `;
 
 const Heading5 = styled.h5`
-    display: block;
-    font-size: 0.83em;
-    margin-bottom: 1.67em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 0.83rem;
+  margin-bottom: 1.67rem;
+  font-weight: 700;
 `;
 
 const Heading6 = styled.h6`
-    display: block;
-    font-size: 0.67em;
-    margin-bottom: 2.33em;
-    font-weight: 700;
-    color: #000;
+  display: block;
+  font-size: 0.67rem;
+  margin-bottom: 2.33rem;
+  font-weight: 700;
 `;
 
 const Paragraph = styled.p`
-    font-size: 1em;
-    margin-bottom: 1em;
-    font-weight: 400;
-    color: #1a1a1a;
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  font-weight: 400;
+  color: #1a1a1a;
+
+  max-width: 65ch;
 `;
 
-export { Heading1, Heading2, Heading3, Heading4, Heading5, Heading6, Paragraph };
+export {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Paragraph,
+};
