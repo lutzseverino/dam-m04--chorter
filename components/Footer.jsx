0a37ac3413eb81e87158abdd74a4b8b27a2e9a0c
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2023-01-30 23:47:42 +0100

    feat: use grid in footer (unfinished)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,72 +1,59 @@
 import styled from "styled-components";
-import Image from "next/image";
 
-import { H1, H2 } from "./typography/Headings";
+import { H2, H3 } from "./typography/Headings";
 import UL from "./typography/UnorderedList";
+import A from "./typography/Link";
 
 const StyledFooter = styled.footer`
   display: grid;
-  place-items: left;
-  grid-gap: 1rem;
-  padding: 2rem 6rem;
-  background-color: #494a5c;
+
+  padding: 32px 96px;
+
+  grid-template-rows: 1fr 1fr;
+
+  background-color: #2b2d42;
   color: white;
-  width: 100%;
-  
-  & > ul {
-  display: grid;
-  flex-direction: column;
-
-  & > li {
-    list-style: none;
-  }
-  }
-  a {
-  box-shadow: inset 0 0 0 0 #ef223c;
-  color: #ef233c;
-  padding: 0 .25rem;
-  margin: 0 -.25rem;
-  transition: color .3s ease-in-out, box-shadow .8s ease-in-out;
-}
-a:hover {
-  color: #fff;
-  box-shadow: inset 200px 0 0 0 #ef223c;;
-}
-
-/* Presentational styles */
-a {
-  color: #000000;
-  font-family: 'Poppins', sans-serif;
-  font-weight: 700;
-  font-size: 1rem;
-  line-height: 1.5;
-  text-decoration: none;
-}
 `;
 
-const Footer = () => {
+const FooterNav = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+function Footer() {
   return (
     <StyledFooter>
-      <H1 href="/">Chores made shorter</H1>
-      <hr />
-      <UL>
-        <li>
-          <H2>Legal</H2>
-        </li>
-        <li>
-          <a href="/privacy">Privacy</a>
-        </li>
-      </UL>
-      <UL>
-        <li>
-          <H2>Company</H2>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-      </UL>
+      <H2>Chores made shorter</H2>
+      <FooterNav>
+        <div>
+          <H3>Legal</H3>
+          <UL>
+            <li>
+              <A href="/legal/privacy">Privacy</A>
+            </li>
+          </UL>
+        </div>
+        <div>
+          <H3>Company</H3>
+          <UL>
+            <li>
+              <A href="/about">About</A>
+            </li>
+          </UL>
+        </div>
+        <div>
+          <H3>Lorem</H3>
+          <UL>
+            <li>
+              <A>Ipsum</A>
+              <A>Dolor</A>
+            </li>
+          </UL>
+        </div>
+      </FooterNav>
     </StyledFooter>
   );
-};
+}
 
 export default Footer;
