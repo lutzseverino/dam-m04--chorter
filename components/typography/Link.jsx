504e481a2348499997ba913daf2697f9557c88ad
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2023-01-31 01:29:26 +0100

    fix: discard animated a tag

--- components/typography/Link.jsx
+++ components/typography/Link.jsx
@@ -3,16 +3,7 @@ import styled from "styled-components";
 const A = styled.a`
   cursor: pointer;
 
-  box-shadow: inset 0 0 0 0 #ef223c;
-  color: #ef233c;
-  padding: 0 0.25rem;
-  margin: 0 -0.25rem;
-  transition: color 0.3s ease-in-out, box-shadow 0.8s ease-in-out;
-
-  &:hover {
-    color: #fff;
-    box-shadow: inset 200px 0 0 0 #ef223c;
-  }
+  text-decoration: none;
 `;
 
 export default A;
